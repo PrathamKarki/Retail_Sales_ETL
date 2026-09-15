7782919929dcfca7fa6bb303b989bb6782a7b65c
Pratham Karki <you@example.com>
2026-09-15 14:14:59 +0545

    Created FactTable with constraint and related foreign keys

---
+++ SQL_Scripts/02_Dimensions/04_Create_FactSales_table.sql
@@ -0,0 +1,22 @@
+USE RetailDW;
+
+GO 
+
+CREATE TABLE FactSales(
+	SalesKey INT IDENTITY(1,1) PRIMARY KEY, 
+	SaleID VARCHAR(50) NOT NULL UNIQUE, 
+	SaleDate DATE NOT NULL, 
+	CustomerKey INT NOT NULL, 
+	ProductKey INT NOT NULL, 
+	StoreKey INT NOT NULL, 
+	Quantity INT NOT NULL, 
+	UnitPrice DECIMAL(18,2) NOT NULL, 
+	TotalAmount DECIMAL(18,2) NOT NULL, 
+	LoadDate DATETIME2 NOT NULL DEFAULT GETDATE(),
+
+	CONSTRAINT FK_FactSales_DimCustomer FOREIGN KEY (CustomerKey) REFERENCES DimCustomer(CustomerKey), 
+	CONSTRAINT FK_FactSales_DimProduct FOREIGN KEY (ProductKey) REFERENCES DimProduct(ProductKey),
+
+	CONSTRAINT FK_FactSales_DimStore FOREIGN KEY (StoreKey) REFERENCES DimStore(StoreKey)
+);
+	
